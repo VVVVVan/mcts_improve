6996f1a2ac7d2653284f1c58be7658f1fbabb732
fshen <shenfan3698@gmail.com>
2019-11-23 17:09:44 -0500

    Modify according to the structure change in othello dir

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ import Arena
 from MCTS import MCTS
 from othello.OthelloGame import OthelloGame
 from othello.OthelloPlayers import RandomPlayer, GreedyOthelloPlayer
-from othello.pytorch.NNet import NNetWrapper as NNet
+from othello.NNet import NNetWrapper as NNet
 import numpy as np
 from utils import *
 
